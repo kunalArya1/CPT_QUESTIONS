512b69c946b4c00c38405dc7525006760abcb264
kunalArya1 <kunalkrraj220@gmail.com>
2022-09-25 09:04:47 +0530

    sum added

---
+++ BASIC/sum.java
@@ -0,0 +1,12 @@
+package BASIC;
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two No : ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum of given no is : "+(a+b));
+    }
+}
